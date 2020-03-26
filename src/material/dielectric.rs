eff2716b6da36281366c63ea0969ab69cbc76d6e
Stepan Stolyarov <stepan.stolyarov@gmail.com>
2020-03-26 20:18:26 +0700

    Schlick's approximation to Fresnel factor
    https://en.wikipedia.org/wiki/Schlick%27s_approximation

--- src/material/dielectric.rs
+++ src/material/dielectric.rs
@@ -24,10 +24,34 @@ impl Material for Dielectric {
         let scattered = if etai_over_etat * sin_theta > 1.0 {
             reflect(unit_direction, rec.n)
         } else {
-            refract(unit_direction, rec.n, etai_over_etat)
+            let reflect_prob = schlick(cos_theta, etai_over_etat);
+            use rand::prelude::*;
+            let mut rng = rand::thread_rng();
+            if rng.gen::<Float>() < reflect_prob {
+                reflect(unit_direction, rec.n)
+            } else {
+                refract(unit_direction, rec.n, etai_over_etat)
+            }
         };
         Some(Ray::new(rec.p, scattered))
     }
+}
+
+// FIXME Float -> f64?
+fn schlick(cosine: Float, refraction_index: Float) -> Float {
+    // ```cpp
+    // auto r0 = (1-ref_idx) / (1+ref_idx);
+    // r0 = r0*r0;
+    // return r0 + (1-r0)*pow((1 - cosine),5);
+    // ```
+
+    let r0 = (1.0 - refraction_index) / (1.0 + refraction_index);
+    let r0 = r0 * r0;
+    r0 + (1.0 - r0) * (1.0 - cosine).powf(5.0)
+
+    // let c = 1.0 - cosine;
+    // let c2 = c * c;
+    // let c5 = c2 * c2 * c; // c.powf(5.0)
 }
 
 // TODO: move `reflect` to Vec3

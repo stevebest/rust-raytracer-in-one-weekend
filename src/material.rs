7446d8ca4ff416bfd3d998afb92807174385ff8c
Stepan Stolyarov <stepan.stolyarov@gmail.com>
2020-03-30 13:05:21 +0700

    Re-import all material types for convenience

--- src/material.rs
+++ src/material.rs
@@ -2,6 +2,10 @@ pub mod dielectric;
 pub mod lambertian;
 pub mod metal;
 
+pub use dielectric::*;
+pub use lambertian::*;
+pub use metal::*;
+
 use crate::geo::{Ray, Vec3f};
 use crate::hit::HitStruct;
 

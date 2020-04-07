f63844b8e7d1585da583cc434391e3b2203aabe0
Stepan Stolyarov <stepan.stolyarov@gmail.com>
2020-04-07 23:25:03 +0700

    Simplify `NullMaterial` implementation
    Just don't scatter any rays. That's it.

--- src/material/null.rs
+++ src/material/null.rs
@@ -6,8 +6,7 @@ use crate::hit::*;
 pub struct NullMaterial;
 
 impl Material for NullMaterial {
-    fn scatter(&self, _ray: &Ray, _hit: &HitStruct<'_>, attenuation: &mut Vec3f) -> Option<Ray> {
-        *attenuation = vec3(0.0, 0.0, 0.0);
-        Some(Ray::new(point3(0.0, 0.0, 0.0), vec3(0.0, 0.0, 0.0)))
+    fn scatter(&self, _ray: &Ray, _hit: &HitStruct<'_>, _attenuation: &mut Vec3f) -> Option<Ray> {
+        None
     }
 }
